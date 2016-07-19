6261ced1a5dfde8fef055ede7af2887bd0c8287e
Sohan Chowdhuruy <sifat3d@gmail.com>
2016-07-19 23:25:23 +0600

    Add JS based url shortener(redirector), very hacky code.

---
+++ redirectorDB.js
@@ -0,0 +1,20 @@
+//All links NEED http:// in-front to work
+//Links with special chars need to be in single quotes
+
+var redirectorDB = {
+"git"   : 'https://github.com/SohanChy?tab=repositories',
+"code"  : 'https://github.com/SohanChy?tab=repositories',
+"gists"   : 'https://gist.github.com/SohanChy',
+"fb"    : 'https://fb.me/sifat3d',
+"twitter" : 'https://twitter.com/iam_sohan',
+"blog" : 'https://blog.sohanchy.com',
+"cloud" : 'https://drive.google.com/folderview?id=0B6K4wLxlfQugdWVGTUJnakd5WTA&usp=sharing',
+"ea"    : 'https://drive.google.com/folderview?id=0B6K4wLxlfQugOU5Jd19XSFVpYXc&usp=sharing',
+
+"cbwin" : 'http://sourceforge.net/projects/codeblocks/files/Binaries/13.12/Windows/codeblocks-13.12mingw-setup.exe/download',
+
+"ks" : 'http://172.27.27.83/home/index.php',
+
+"omaigawdgif" : 'https://brafootball.files.wordpress.com/2013/12/bwuhhh-gif-1.gif?w=830',
+"basegaaps" : 'https://www.androidfilehost.com/?fid=24421527759887409',
+}

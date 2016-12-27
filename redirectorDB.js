ac3bd76746d2d4f8a38cb9835a96c553fda030ba
Sohan Chowdhury <sifat3d@gmail.com>
2016-12-27 22:03:08 +0600

    Add Temporary Public Uploads to RedirectorDB
    remove obsolete link.

--- redirectorDB.js
+++ redirectorDB.js
@@ -9,19 +9,17 @@ var redirectorDB = {
 "fb"    : 'https://fb.me/sifat3d',
 "twitter" : 'https://twitter.com/iam_sohan',
 "blog" : 'https://blog.sohanchy.com',
-"cloud" : 'https://drive.google.com/folderview?id=0B6K4wLxlfQugdWVGTUJnakd5WTA&usp=sharing',
-"ea"    : 'https://drive.google.com/folderview?id=0B6K4wLxlfQugOU5Jd19XSFVpYXc&usp=sharing',
+"cloud" : 'https://drive.google.com/embeddedfolderview?id=0B6K4wLxlfQugdWVGTUJnakd5WTA#grid',
+"tpus" : 'https://drive.google.com/embeddedfolderview?id=0B6K4wLxlfQugQVFhNHFycjl5YzQ#grid',
+"ea"    : 'https://drive.google.com/embeddedfolderview?id=0B6K4wLxlfQugOU5Jd19XSFVpYXc#grid',
 
 "pscookbook" : 'https://docs.google.com/document/d/1HE1NVEGxx8mScyQp4qrhwsAIVtRV1XnfggxPWJ38Cu0/edit?usp=sharing',
 
 "cn" : 'https://drive.google.com/drive/folders/0B6K4wLxlfQugMDEybm1NTXlfUUk',
-"cbwin" : 'http://sourceforge.net/projects/codeblocks/files/Binaries/13.12/Windows/codeblocks-13.12mingw-setup.exe/download',
-
 "ks" : 'http://172.27.27.83/home/index.php',
 
 "omaigawdgif" : 'https://brafootball.files.wordpress.com/2013/12/bwuhhh-gif-1.gif?w=830',
 "woof" : 'https://www.reddit.com/r/woof_irl/top/?sort=top&t=all',
 
-"basegaaps" : 'https://www.androidfilehost.com/?fid=24421527759887409',
 }
 
